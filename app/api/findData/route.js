01d1a15ad1fdfede25fff6fd800830f1db932824
username1111111111 <username1111111111@gmail.com>
2024-02-17 23:06:48 +0400

    Fixes

--- app/api/findData/route.js
+++ app/api/findData/route.js
@@ -10,37 +10,70 @@ async function handler(req) {
 
         let client;
         let collection;
-        
+
         try {
             client = await getClient();
             collection = await getCollection(client);
         } catch (error) {
-            console.error("Error connecting to the database in findData.js:", error);
+            console.error(
+                "Error connecting to the database in findData.js:",
+                error
+            );
             return res
                 .status(500)
-                .json({ message: "Error connecting to the database in findData.js" });
+                .json({
+                    message: "Error connecting to the database in findData.js",
+                });
         }
 
         try {
             const findQuery = { [providedKey]: providedData };
-            const user = await collection.findOne(findQuery);
 
-            console.log(`USER in findData: -----> ${user.email}`);
+            try {
+                const user = await collection.findOne(findQuery);
+                console.log(`USER in findData: -----> ${user.email}`);
+            } catch(error) {
+                console.error(
+                    `Error getting user from MONGO in findData.js: ${error.message}`
+                );
+            }
+
 
             if (!user) {
                 // User not found
-                return res.status(404).json({ message: "User not found in findData.js" });
+                const res = new Response(null, {
+                    status: 404,
+                    statusText: "User not found",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                return res;
             }
 
             console.log(`USER in findData: -----> ${user.email}`);
-            return res.status(200).json(user);
+            const resBody = JSON.stringify(user);
+
+            const res = new Response(resBody, {
+                status: 200,
+                statusText: "User have been found",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            return res;
         } catch (error) {
             console.error(
                 `Error fetching user data in findData.js: ${error.message}`
             );
-            return res.status(500).json({
-                message: `Error fetching user data in findData.js: ${error.message}`,
+            const res = new Response(null, {
+                status: 500,
+                statusText: `Error fetching user data in findData.js: ${error.message}`,
+                headers: {
+                    "Content-Type": "application/json",
+                },
             });
+            return res;
         } finally {
             if (client) {
                 await client.close();
